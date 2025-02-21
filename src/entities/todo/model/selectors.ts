e4d9903f08054c498c64c7821a86e55c3235adf5
Aman <amanbakytov00@gmail.com>
2025-02-21 19:58:52 +0600

    update selector

--- src/entities/todo/model/selectors.ts
+++ src/entities/todo/model/selectors.ts
@@ -1,19 +1,20 @@
 import { RootState } from "@app/store";
+import { createSelector } from "@reduxjs/toolkit";
 
 export const selectTodos = (state: RootState) => state.todos.todos;
 export const selectFilter = (state: RootState) => state.todos.filter;
 export const selectLoading = (state: RootState) => state.todos.loading;
 
-export const selectFilteredTodos = (state: RootState) => {
-  const todos = selectTodos(state);
-  const filter = selectFilter(state);
-
-  switch (filter) {
-    case "active":
-      return todos.filter((todo) => !todo.completed);
-    case "completed":
-      return todos.filter((todo) => todo.completed);
-    default:
-      return todos;
+export const selectFilteredTodos = createSelector(
+  [selectTodos, selectFilter],
+  (todos, filter) => {
+    switch (filter) {
+      case "active":
+        return todos.filter((todo) => !todo.completed);
+      case "completed":
+        return todos.filter((todo) => todo.completed);
+      default:
+        return todos;
+    }
   }
-};
+);
